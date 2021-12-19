a43d9c0ff226c953821a77210e24d8e3956e9491
abeforgit <arnebertrand@gmail.com>
2021-12-20 00:13:24 +0100

    add more sxhkd shortcuts

--- modules/bspwm.nix
+++ modules/bspwm.nix
@@ -14,6 +14,20 @@ in {
 				enable = true;
 				windowManager.bspwm = {
 					enable = true;
+					settings = {
+						border_width = 10;
+						window_gap = 12;
+						split_ratio = 0.52;
+						borderless_monocle = true;
+						gapless_monocle = true;
+						focus_follows_pointer = true;
+						pointer_follows_monitor = true;
+
+					};
+					monitors = {
+						"^1" = [ "I" ];
+						"^2" = [ "II" ];
+					};
 				};
 				scriptPath = ".hm-xsession";
 			};
@@ -23,6 +37,9 @@ in {
 					"super + Return" = "kitty";
 					"super + {_,shift + } {h,j,k,l}" = "bspc node -{f, s} {west, south, north, east}";
 					"super + shift + q" = "bspc node -c";
+					"super + {1-2}" = "bspc desktop -f '{I,II}'";
+					"super + d" = "rofi -show combi";
+					"super + space; r; s" = "pkill -USR1 -x sxhkd";
 				};
 			};
 		};
