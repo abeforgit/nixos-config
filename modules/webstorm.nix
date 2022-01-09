54363311b813e7d17e99e0494105ae5f33417681
abeforgit <arnebertrand@gmail.com>
2022-01-10 00:59:34 +0100

    Use the explicit sdk approach for webstorm

--- modules/webstorm.nix
+++ modules/webstorm.nix
@@ -9,33 +9,31 @@ in {
     };
   };
   config = mkIf cfg.enable {
-    home-manager.users.${config.custom.user} = { pkgs, ... }: {
-      home.packages = with pkgs; [ nodejs nodejs-14_x jetbrains.webstorm ];
-    };
 
-    # for future reference
-    #   home-manager.users.${config.custom.user} = let
-    #     devSDKs = with pkgs; {
-    #     };
-    #     extraPath = makeBinPath (builtins.attrValues devSDKs);
-    #     webstorm = pkgs.runCommand "webstorm" {
-    #       nativeBuildInputs = [ pkgs.makeWrapper ];
-    #     } ''
-    #       mkdir -p $out/bin
-    #       makeWrapper ${pkgs.jetbrains.webstorm}/bin/webstorm \
-    #         $out/bin/webstorm \
-    #         --prefix PATH : ${extraPath}
-    #     '';
-    #   in { ... }: {
-    #     home.packages = [ webstorm ];
-    #     home.file.".local/dev".source = let
-    #       mkEntry = name: value: {
-    #         inherit name;
-    #         path = value;
-    #       };
-    #       entries = mapAttrsToList mkEntry devSDKs;
-    #     in pkgs.linkFarm "local-dev" entries;
-    #   };
+    home-manager.users.${config.custom.user} = let
+      devSDKs = with pkgs; {
+        node = nodejs;
+        node14 = nodejs-14_x;
+      };
+      extraPath = makeBinPath (builtins.attrValues devSDKs);
+      webstorm = pkgs.runCommand "webstorm" {
+        nativeBuildInputs = [ pkgs.makeWrapper ];
+      } ''
+              mkdir -p $out/bin
+              makeWrapper ${pkgs.jetbrains.webstorm}/bin/webstorm \
+                $out/bin/webstorm \
+                --prefix PATH : ${extraPath}
+        #     '';
+    in { ... }: {
+      home.packages = [ webstorm ];
+      home.file.".local/webstorm-dev".source = let
+        mkEntry = name: value: {
+          inherit name;
+          path = value;
+        };
+        entries = mapAttrsToList mkEntry devSDKs;
+      in pkgs.linkFarm "local-dev" entries;
+    };
   };
 
 }
