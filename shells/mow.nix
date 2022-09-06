aea235e4ca7cc9786bd30efdca80362be54c6f7c
abeforgit <arnebertrand@gmail.com>
2022-09-06 17:58:34 +0200

    Add mow project shell

---
+++ shells/mow.nix
@@ -0,0 +1,52 @@
+{pkgs, inputs}:
+let
+  node_package = pkgs.nodejs-16_x;
+  npm-global = toString ~/.npm-global;
+  ember = "${npm-global}/bin/ember";
+  name = "gn-meta";
+  root = ''"$PRJ_ROOT"'';
+in pkgs.devshell.mkShell {
+  inherit name;
+  env = [
+    {
+      name = "NPM_CONFIG_PREFIX";
+      value = npm-global;
+    }
+    {
+      name = "PATH";
+      prefix = "${npm-global}/bin";
+    }
+    {
+      name = "COMPOSE_FILE";
+      value =
+        "docker-compose.yml:docker-compose.dev.yml:docker-compose.override.yml";
+    }
+    {
+      name = "WOODPECKER_SERVER";
+      value = "https://build.redpencil.io";
+    }
+  ];
+  commands = [
+    {
+      name = "npm";
+      help = "The node package manager";
+      package = node_package;
+    }
+    {
+      name = "node";
+      help = "Nodejs";
+      package = node_package;
+    }
+    {
+      name = "ember";
+      help = "The ember cli";
+      command = ''${ember} "$@"'';
+    }
+    # {
+    #   name = "woodpecker-cli";
+    #   help = "The Woodpecker cli";
+    #   package = pkgs.woodpecker-cli;
+    # }
+
+  ];
+}
