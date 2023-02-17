a075f3b5ddc9fd31cd9dac60cf885dd84acbc1df
abeforgit <arnebertrand@gmail.com>
2023-02-17 15:27:32 +0100

    Add more caches

--- machines/finch/default.nix
+++ machines/finch/default.nix
@@ -66,8 +66,18 @@ in {
   # };
 
   nix.settings = {
-    substituters = ["https://hyprland.cachix.org" "https://nix-community.cachix.org"];
-    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
+    substituters = [
+    "https://hyprland.cachix.org"
+    "https://nix-community.cachix.org"
+    "https://cache.nixos.org"
+    "https://cachix.cachix.org"
+    ];
+    trusted-public-keys = [
+    "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
+    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    ];
   };
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.extraLocaleSettings = {
