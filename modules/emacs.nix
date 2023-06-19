671fad1c9aa17a778608978f5fc808cb97fcdcb7
abeforgit <arnebertrand@gmail.com>
2023-06-20 00:29:11 +0200

    fix emacs to not segfault anymore

--- modules/emacs.nix
+++ modules/emacs.nix
@@ -6,6 +6,16 @@ let
     ((emacsPackagesFor emacs).emacsWithPackages (epkgs: [
       (epkgs.treesit-grammars.with-grammars
         (grammars: [ grammars.tree-sitter-bash ]))
+      epkgs.all-the-icons
+      epkgs.treemacs-all-the-icons
+      epkgs.spaceline-all-the-icons
+      epkgs.all-the-icons-ibuffer
+      epkgs.all-the-icons-ivy
+      epkgs.all-the-icons-ivy-rich
+      epkgs.all-the-icons-gnus
+      epkgs.all-the-icons-dired
+      epkgs.all-the-icons-completion
+
       epkgs.vterm
       epkgs.magit
     ]));
@@ -29,6 +39,9 @@ in {
         fd
         clang
         xclip
+        xdotool
+        xorg.xprop
+        xorg.xwininfo
         shellcheck
         html-tidy
         nodePackages.bash-language-server
@@ -45,6 +58,7 @@ in {
         python3
         google-chrome
         sumneko-lua-language-server
+        emacs-all-the-icons-fonts
       ];
       home.file.lua-ls = {
         source = "${pkgs.sumneko-lua-language-server}";
