0801f6723895a509464cd6de1a9bd2fb29c7a09d
abeforgit <arnebertrand@gmail.com>
2023-06-28 11:16:36 +0200

    Add default nushell conf

--- modules/nushell.nix
+++ modules/nushell.nix
@@ -12,41 +12,42 @@ in {
     home-manager.users.${config.custom.user} = { pkgs, home, ... }: {
       programs.nushell = {
         enable = true;
+        configFile.source = ./config.nu;
 
-        shellAliases = {
-          mux = "tmuxinator";
-          docker-stopall = "docker stop $(docker ps -q)";
-          mucli = "~/repos/redpencil/mu-cli/mu";
-          emc = "emacsclient -nw";
+        # shellAliases = {
+        #   mux = "tmuxinator";
+        #   docker-stopall = "docker stop $(docker ps -q)";
+        #   mucli = "~/repos/redpencil/mu-cli/mu";
+        #   emc = "emacsclient -nw";
 
-          gmdh = ''git log --pretty=format:"%s%n%b" development..HEAD'';
-          gitmsg = ''git log --pretty=format:"%s%n%b'';
-          ls = "lsd";
-          lt = "lsd --tree";
-          grep = "grep --colour=auto";
-          egrep = "egrep --colour=auto";
-          fgrep = "fgrep --colour=auto";
-          cp = "cp -i";
-          df = "df -h";
-          free = "free -m";
-          ipa = "ip -c=auto a";
-          ip4 = "ip -c=auto -br -4 a";
-          ip6 = "ip -c=auto -br -6 a";
-          lsizes = "sudo du -hsx .[!.]* * | sort -rh";
-          pgrep = "pgrep -l";
-          gits = "git status";
-          gg =
-            "git log --graph --pretty=format:'%C(bold)%h%Creset%C(magenta)%d%Creset %C(yellow)<%an> %C(cyan)(%cr)%Creset' --abbrev-commit --date=relative";
-          gc = "git checkout";
-          ghprc = "gh pr ls | fzf | cut -f 1 | xargs gh pr checkout";
-          extip = "curl ifconfig.co";
-          icat = "kitty +kitten icat";
-          rebuild =
-            "nixos-rebuild --flake ~/repos/nixos-config#finch switch --use-remote-sudo";
-          checkbuild =
-            "nixos-rebuild --flake ~/repos/nixos-config#finch build && nvd diff /run/current-system ~/repos/nixos-config/result";
-          srcrc = "source ~/.zshrc";
-        };
+        #   gmdh = ''git log --pretty=format:"%s%n%b" development..HEAD'';
+        #   gitmsg = ''git log --pretty=format:"%s%n%b'';
+        #   ls = "lsd";
+        #   lt = "lsd --tree";
+        #   grep = "grep --colour=auto";
+        #   egrep = "egrep --colour=auto";
+        #   fgrep = "fgrep --colour=auto";
+        #   cp = "cp -i";
+        #   df = "df -h";
+        #   free = "free -m";
+        #   ipa = "ip -c=auto a";
+        #   ip4 = "ip -c=auto -br -4 a";
+        #   ip6 = "ip -c=auto -br -6 a";
+        #   lsizes = "sudo du -hsx .[!.]* * | sort -rh";
+        #   pgrep = "pgrep -l";
+        #   gits = "git status";
+        #   gg =
+        #     "git log --graph --pretty=format:'%C(bold)%h%Creset%C(magenta)%d%Creset %C(yellow)<%an> %C(cyan)(%cr)%Creset' --abbrev-commit --date=relative";
+        #   gc = "git checkout";
+        #   ghprc = "gh pr ls | fzf | cut -f 1 | xargs gh pr checkout";
+        #   extip = "curl ifconfig.co";
+        #   icat = "kitty +kitten icat";
+        #   rebuild =
+        #     "nixos-rebuild --flake ~/repos/nixos-config#finch switch --use-remote-sudo";
+        #   checkbuild =
+        #     "nixos-rebuild --flake ~/repos/nixos-config#finch build and nvd diff /run/current-system ~/repos/nixos-config/result";
+        #   srcrc = "source ~/.zshrc";
+        # };
       };
       programs.starship = {
         enable = true;
@@ -60,7 +61,6 @@ in {
       programs.zoxide = {
         enable = true;
         enableNushellIntegration = true;
-        options = [ "--cmd j" ];
       };
       programs.fzf = {
         enable = true;
