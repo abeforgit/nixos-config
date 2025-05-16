f7d223e003777d8b9d758835afa746e1640100c2
abeforgit <arnebertrand@gmail.com>
2025-05-16 10:27:44 +0200

    expand auto-save config

--- nvim/config/plugins/auto-save.lua
+++ nvim/config/plugins/auto-save.lua
@@ -1,16 +1,47 @@
+local excluded_filetypes = {
+  "gitcommit",
+  -- should normally be non-modifiable, but just in case
+  "NvimTree",
+  "Outline",
+  "TelescopePrompt",
+  "alpha",
+  "dashboard",
+  "lazygit",
+  "neo-tree",
+  "oil",
+  "prompt",
+  "toggleterm",
+}
+local excluded_filenames = {
+}
+
+local save_condition = function(buf)
+  local fn = vim.fn
+  local utils = require("auto-save.utils.data")
+
+  if
+      utils.not_in(fn.getbufvar(buf, "&filetype"), excluded_filetypes)
+      and utils.not_in(fn.expand("%:t"), excluded_filenames)
+  then
+    return true -- met condition(s), can save
+  end
+  return false  -- can't save
+end
+
+
 return {
   "okuuva/auto-save.nvim",
   version = '*',
   cmd = "ASToggle",
-  event = { "InsertLeave", "TextChanged" },
+  event = { "InsertLeave", "TextChanged", "BufLeave", "FocusLost" },
   opts = {
-    condition = function(buf)
-      local filetype = vim.fn.getbufvar(buf, "&filetype")
-      if vim.list_contains({ "gitcommit" }, filetype) then
-        return false
-      end
-      return true
-    end
+    enabled = true,
+    trigger_events = {                          -- See :h events
+      immediate_save = immediate_triggers,      -- vim events that trigger an immediate save
+      defer_save = deferred_triggers,           -- vim events that trigger a deferred save (saves after `debounce_delay`)
+      cancel_deferred_save = { "InsertEnter" }, -- vim events that cancel a pending deferred save
+    },
+    condition = save_condition,
+    debounce_delay = 1000
   },
-  debounce_delay = 500
 }
