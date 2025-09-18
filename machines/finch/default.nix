ae6d9d6afc0cba977d7c9e5c0ee301e86a97d332
abeforgit <arnebertrand@gmail.com>
2025-09-18 22:30:32 +0200

    headbuds stuff

--- machines/finch/default.nix
+++ machines/finch/default.nix
@@ -398,8 +398,26 @@ in
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
-    package = pkgs.bluez;
+    settings = {
+      General = {
+        # Shows battery charge of connected devices on supported
+        # Bluetooth adapters. Defaults to 'false'.
+        Experimental = true;
+        # When enabled other devices can connect faster to us, however
+        # the tradeoff is increased power consumption. Defaults to
+        # 'false'.
+        FastConnectable = true;
+      };
+      Policy = {
+        # Enable all controllers when they are found. This includes
+        # adapters present on start as well as adapters that are plugged
+        # in later on. Defaults to 'true'.
+        AutoEnable = true;
+      };
+    };
+    # package = pkgs.bluez;
   };
+  services.blueman.enable = true;
   # services.blueman = { enable = true; };
   # specialisation = {
   # 	external-display.configuration = {
