284d79083c64ab1ac449178c311f7f0a9dee284e
abeforgit <arnebertrand@gmail.com>
2026-08-19 11:03:56 +0200

    update flake

--- nvim/config/plugins/ember.lua
+++ nvim/config/plugins/ember.lua
@@ -1,10 +1,19 @@
 return {
   "NullVoxPopuli/ember.nvim",
-  dependencies = {"neovim/nvim-lspconfig", "nvim-treesitter/nvim-treesitter" },
+  dependencies = { "neovim/nvim-lspconfig", "nvim-treesitter/nvim-treesitter" },
 
   lazy = false,
   config = function()
     require('ember.nvim').config()
+    vim.lsp.config('ts_ls', {
+      init_options = {
+        preferences = {
+          includeCompletionsForModuleExports = true,
+          includeCompletionsForImportStatements = true,
+          importModuleSpecifierPreference = "shortest"
+        }
+      }
+    })
   end
 
 }
