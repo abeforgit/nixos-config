d6899560218a39b35d1ac2c448485a04ea892b67
abeforgit <arnebertrand@gmail.com>
2026-09-15 14:46:32 +0200

    dms

--- modules/niri.nix
+++ modules/niri.nix
@@ -56,12 +56,10 @@ in
       };
 
       # Core features
-      enableSystemMonitoring = true; # System monitoring widgets (dgop)
       enableVPN = true; # VPN management widget
       enableDynamicTheming = true; # Wallpaper-based theming (matugen)
       enableAudioWavelength = true; # Audio visualizer (cava)
       enableCalendarEvents = true; # Calendar integration (khal)
-      enableClipboardPaste = true; # Pasting from the clipboard history (wtype)
     };
     programs.dsearch.enable = true;
     home-manager.users.${config.custom.user} =
